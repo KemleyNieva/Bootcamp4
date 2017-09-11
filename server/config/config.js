4a21fa30312a7d1abcb8210f8461ef030658fb65
Kemley <kemley@ufl.edu>
2017-09-11 16:06:27 -0400

    Initial commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb://user1:user1@ds119014.mlab.com:19014/bootcamp3', //place the URI of your mongo database here.
+  },
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
